3c092d6dd8f38f461d7353d2564a7636fe1e5a6b
Joshua Westbrook <profdeadpool@gmail.com>
2016-09-25 21:53:19 -0400

    Right click to move agent and agent can now jump to all walls

--- Assets/Scripts/fullAgent.cs
+++ Assets/Scripts/fullAgent.cs
@@ -5,12 +5,21 @@ public class fullAgent : MonoBehaviour {
 
 	Transform target;
 	NavMeshAgent agent;
+	private Ray shootRay;
 
-	void Start () {
+	void Awake () {
 		agent = GetComponent<NavMeshAgent>();
 	}
 
 	void Update () {
-		agent.SetDestination(target.position);
+		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+		RaycastHit hit;
+		if (Input.GetButtonDown ("Fire2")) {
+			if (Physics.Raycast(ray, out hit, 100)) {
+				target = hit.transform;
+				agent.SetDestination(hit.point);
+				agent.Resume();
+			}
+		}
 	}
 }
