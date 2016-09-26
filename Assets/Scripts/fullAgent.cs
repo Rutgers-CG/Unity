496fa6c36ddbb86cb50102fe280c376ae59ed8dd
Joshua Westbrook <profdeadpool@gmail.com>
2016-09-25 21:17:50 -0400

    Changed the NavMesh so agents could climb properly

---
+++ Assets/Scripts/fullAgent.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fullAgent : MonoBehaviour {
+
+	Transform target;
+	NavMeshAgent agent;
+
+	void Start () {
+		agent = GetComponent<NavMeshAgent>();
+	}
+
+	void Update () {
+		agent.SetDestination(target.position);
+	}
+}
