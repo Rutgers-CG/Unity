e7fb200aad48a8f3d61be7b12e5b031cf101a4aa
Bingchen Liu <bruce.bc.liu@outlook.com>
2016-09-27 14:57:40 -0400

    auto move obstacles

---
+++ Assets/Scripts/MoveBetweenTwoPoints.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveBetweenTwoPoints : MonoBehaviour {
+
+    public Vector3 pos1;
+    public Vector3 pos2;
+    public float speed = 1.0f;
+
+    void Update()
+    {
+        transform.position = Vector3.Lerp(pos1, pos2, (Mathf.Sin(speed * Time.time) + 1.0f) / 2.0f);
+    }
+}
