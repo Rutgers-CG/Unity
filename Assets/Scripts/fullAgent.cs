f60442f33ef59a234348aad35fb4328c4f0ba509
Bingchen Liu <bruce.bc.liu@outlook.com>
2016-09-28 00:34:22 -0400

    avoid collision for agents in reaching the target

--- Assets/Scripts/fullAgent.cs
+++ Assets/Scripts/fullAgent.cs
@@ -4,20 +4,49 @@ using System.Collections;
 public class fullAgent : MonoBehaviour {
 
 	Vector3 target;
+
 	NavMeshAgent agent;
+	NavMeshObstacle obs;
+
 	private Ray shootRay;
 	bool isSelected;
 
-	//set the initial goal of travel to be not moving anywhere
-	void Awake () {
+	//bool isInMotion = false;
+	bool isArrived = true;
+
+    public static float nbrInTarget = 0;
+	public static int selectedTotal = 0;
+
+	public void stop(){
+		agent.Stop();
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+		if (!isArrived) {
+			agent.Stop ();
+			Debug.Log ("hitted by agent");
+		}
+    }
+
+    //set the initial goal of travel to be not moving anywhere
+    void Awake () {
 		agent = GetComponent<NavMeshAgent>();
+		obs = GetComponent<NavMeshObstacle>();
+
 		target = gameObject.transform.position;
 	}
 
 	//sends the agent towards the given goal of travel
 	void Update () {
-		agent.SetDestination (target);
-		agent.Resume ();
+		//agent.SetDestination (target);
+		if (Vector3.Distance (transform.position, target) < 2.0f) {
+			agent.enabled = false;
+			obs.enabled = true;
+		} else {
+			agent.enabled = true;
+			obs.enabled = false;
+		}
 	}
 
 	//used by director to toggle the agent being selected on
@@ -25,6 +54,8 @@ public class fullAgent : MonoBehaviour {
 		Renderer rend = GetComponent<Renderer> ();
 		rend.material.color = Color.blue;
 		isSelected = true;
+		selectedTotal += 1;
+		//Debug.Log (selectedTotal);
 	}
 
 	//used by director to toggle the agent being selected off
@@ -32,6 +63,7 @@ public class fullAgent : MonoBehaviour {
 		Renderer rend = GetComponent<Renderer> ();
 		rend.material.color = Color.white;
 		isSelected = false;
+		selectedTotal -= 1;
 	}
 
 	public bool getIsSelected() {
@@ -41,5 +73,11 @@ public class fullAgent : MonoBehaviour {
 	//used by Director to set the location of travel
 	public void setTarget(RaycastHit givenHit) {
 		target = givenHit.point;
+
+		isArrived = false;
+		agent.enabled = true;
+		obs.enabled = false;
+		agent.SetDestination (target);
+
 	}
 }
