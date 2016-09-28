35941cf94ff5a9ce15a69ae73546b278bfc89269
Brandon <b.c.dunlap@outlook.com>
2016-09-27 20:57:35 -0400

    Camera Control
    Camera Movement and horizontal rotation

---
+++ Assets/Scripts/NavCharScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NavCharScript : MonoBehaviour
+{
+
+    NavMeshAgent agent;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                agent.SetDestination(hit.point);
+            }
+        }
+    }
+}
