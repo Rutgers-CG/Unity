fe1b8857a5957764d03dbbf982c22f6b1b0381a1
Joshua Westbrook <profdeadpool@gmail.com>
2016-10-12 22:46:29 -0400

    Now animates walk and run. Jump is still broken

--- Assets/Scripts/MouseCharacterController.cs
+++ Assets/Scripts/MouseCharacterController.cs
@@ -2,19 +2,15 @@
 using System.Collections;
 
 public class MouseCharacterController : MonoBehaviour {
-    Animator anim;
 
 	bool isSelected;
     Vector3 target;
     NavMeshAgent navAgent;
-    int jumpNbr = 0;
     // Use this for initialization
     void Start () {
 
 		isSelected = false;
         navAgent = GetComponent<NavMeshAgent>();
-
-        anim = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
@@ -54,54 +50,8 @@ public class MouseCharacterController : MonoBehaviour {
                     Debug.Log(target);
 					transform.LookAt (target);
                     navAgent.SetDestination(target);
-					/*float zForce = navAgent.desiredVelocity.z;
-					float xForce = navAgent.desiredVelocity.x;
-					anim.SetFloat("zForce", zForce);
-					anim.SetFloat("xForce", xForce);*/
                 }
             }
         }
-
-        // get the off-mesh link, will trigger the jump animation
-        // every time it will go through 2 mesh, so use a number to count
-        if (navAgent.isOnOffMeshLink) {
-            if (jumpNbr % 2 == 0)
-            {
-                Debug.Log("should jump here!");
-                anim.SetTrigger("Jump");
-            }
-            jumpNbr += 1;
-        }
-
-        /*float zForce = Input.GetAxis("Vertical");
-        float xForce = Input.GetAxis("Horizontal");
-        anim.SetFloat("zForce", zForce);
-        anim.SetFloat("xForce", xForce);*/
-        
-		if (Input.GetKey(KeyCode.LeftShift))
-        {
-            anim.SetBool("run", true);
-        }
-        else
-        {
-            anim.SetBool("run", false);
-        }
-        /*if (Input.GetKey(KeyCode.LeftControl) && xForce!=0)
-        {
-            anim.SetBool("strafe", true);
-        }
-        else
-        {
-            if (zForce == 0 && xForce != 0)
-            {
-                transform.Rotate(new Vector3(0, xForce * 60 * Time.deltaTime));
-            }
-            anim.SetBool("strafe", false);
-        }
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            anim.SetTrigger("Jump");
-        }*/
-
     }
 }
---
+++ Assets/Scripts/goblinAnimator.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class goblinAnimator : MonoBehaviour {
+	Animator anim;
+	NavMeshAgent agent;
+	int jumpNbr = 0;
+	Vector2 smoothDeltaPosition = Vector2.zero;
+	Vector2 velocity = Vector2.zero;
+
+	void Start ()
+	{
+		anim = GetComponent<Animator> ();
+		agent = GetComponent<NavMeshAgent> ();
+		// Don’t update position automatically
+		agent.updatePosition = false;
+	}
+
+	void Update ()
+	{
+		Vector3 worldDeltaPosition = agent.nextPosition - transform.position;
+
+		// Map 'worldDeltaPosition' to local space
+		float dx = Vector3.Dot (transform.right, worldDeltaPosition);
+		float dz = Vector3.Dot (transform.forward, worldDeltaPosition);
+		Vector2 deltaPosition = new Vector2 (dx, dz);
+
+		// Low-pass filter the deltaMove
+		float smooth = Mathf.Min(1.0f, Time.deltaTime/0.15f);
+		smoothDeltaPosition = Vector2.Lerp (smoothDeltaPosition, deltaPosition, smooth);
+
+		// Update velocity if time advances
+		if (Time.deltaTime > 1e-5f)
+			velocity = smoothDeltaPosition / Time.deltaTime;
+
+		bool shouldMove = velocity.magnitude > 0.5f && agent.remainingDistance > agent.radius;
+
+		// Update animation parameters
+		anim.SetBool("Moving", shouldMove);
+		anim.SetFloat ("xForce", velocity.x);
+		anim.SetFloat ("zForce", velocity.y);
+
+		//GetComponent<LookAt>().lookAtTargetPosition = agent.steeringTarget + transform.forward;
+
+		if (Input.GetKey(KeyCode.LeftShift))
+		{
+			anim.SetBool("run", true);
+		}
+		else
+		{
+			anim.SetBool("run", false);
+		}
+
+		// get the off-mesh link, will trigger the jump animation
+		// every time it will go through 2 mesh, so use a number to count
+		if (agent.isOnOffMeshLink) {
+			if (jumpNbr % 2 == 0)
+			{
+				Debug.Log("should jump here!");
+				anim.SetTrigger("Jump");
+			}
+			jumpNbr += 1;
+		}
+	}
+
+	void OnAnimatorMove ()
+	{
+		// Update position to agent position
+		transform.position = agent.nextPosition;
+	}
+}
