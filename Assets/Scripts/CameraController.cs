bd9cc0cf2e51cb2d006f539fbedebe73dc135279
AlmightyDucky <cjg233@scarletmail.rutgers.edu>
2017-10-07 21:24:01 -0400

    Uploading mostly completed B1 project. Need to work on Refined Navigation part1: Prevent multiple agents from bunching up, Report questions 1 and 2, create YouTube video, and extra credit if time permitting.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    #region Enum Variables
+
+    public enum KEYBOARD_CONTROLS : int
+    {
+        FOWARD = KeyCode.W,
+        BACKWARD = KeyCode.S,
+        LEFT_STRAFE = KeyCode.A,
+        RIGHT_STRAFE = KeyCode.D,
+        SPEED_MULTIPLY = KeyCode.LeftShift
+    }
+
+    public enum MOUSE_CONTROLS : int
+    {
+        PITCH_MOUSE = KeyCode.Mouse1,
+        YAW_MOUSE = KeyCode.Mouse1
+    }
+
+    #endregion
+
+    #region Variables
+
+    private float speedMultiplier = 4f;
+    [Range(1f, 20f)]public float objectRotationSpeed = 5f;
+    [Range(1f, 20f)] public float cameraRotationThreshold= 0.5f;
+    [Tooltip("Inverts the vertical axis on camera.")]public bool invertVertical = true;
+    [Tooltip("Inverts the horizontal axis on camera.")] public bool invertHorizontal = false;
+
+    private Transform mainCamera;
+
+    private Vector2 lastMousePosition;
+
+    #endregion
+
+    #region CameraController Functions
+
+    public void Start ()
+    {
+        mainCamera = Camera.main.transform;
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        if (Input.GetKey((KeyCode) KEYBOARD_CONTROLS.SPEED_MULTIPLY))
+        {
+           speedMultiplier = 8f;
+        } else {
+           speedMultiplier = 4f;
+        }
+
+        //Handling the keyboard input : translations
+        foreach (KEYBOARD_CONTROLS key in Enum.GetValues(typeof(KEYBOARD_CONTROLS)))
+        {
+            if(Input.GetKey((KeyCode) key))
+            {
+                switch(key)
+                {
+                    // translation movements
+                    case KEYBOARD_CONTROLS.FOWARD:
+                        mainCamera.position += (mainCamera.forward * Time.deltaTime * speedMultiplier);
+                        break;
+                    case KEYBOARD_CONTROLS.BACKWARD:
+                        mainCamera.position -= (mainCamera.forward * Time.deltaTime * speedMultiplier);
+                        break;
+                    case KEYBOARD_CONTROLS.LEFT_STRAFE:
+                        mainCamera.position -= (mainCamera.right * Time.deltaTime * speedMultiplier);
+                        break;
+                    case KEYBOARD_CONTROLS.RIGHT_STRAFE:
+                        mainCamera.position += (mainCamera.right * Time.deltaTime * speedMultiplier);
+                        break;
+                }
+            }
+        }
+
+        //Handling the mouse input : rotations
+        foreach (MOUSE_CONTROLS button in Enum.GetValues(typeof(MOUSE_CONTROLS)))
+        {
+            if (Input.GetKey((KeyCode)button))
+            {
+                float horizontalDelta = lastMousePosition.x - Input.mousePosition.x;
+                float verticalDelta = lastMousePosition.y - Input.mousePosition.y;
+
+                // rotations : rotate horizontally then vertically
+                if(Mathf.Abs(horizontalDelta) > cameraRotationThreshold)
+                {
+                    if (invertHorizontal)
+                    {
+                        horizontalDelta *= -1;
+                    }
+
+                    mainCamera.Rotate(Vector3.up, horizontalDelta * speedMultiplier * objectRotationSpeed * Time.deltaTime, Space.World);
+                }
+
+                if (Mathf.Abs(verticalDelta) > cameraRotationThreshold)
+                {
+                    if (invertVertical)
+                    {
+                        verticalDelta *= -1;
+                    }
+
+                    mainCamera.Rotate(mainCamera.right, verticalDelta * speedMultiplier * objectRotationSpeed * Time.deltaTime, Space.World);
+                }
+
+            }
+        }
+
+        lastMousePosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+    }
+
+    public void ChangeVerticalInvert()
+    {
+        invertVertical = !invertVertical;
+    }
+
+    public void ChangeHorizontalInvert()
+    {
+        invertHorizontal = !invertHorizontal;
+    }
+
+    #endregion
+}
---
+++ Assets/Scripts/MovableObstacleScript.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using UnityEngine;
+
+public class MovableObstacleScript : MonoBehaviour
+{
+    public enum KEYBOARD_CONTROLS : int
+    {
+        FOWARD = KeyCode.UpArrow,
+        BACKWARD = KeyCode.DownArrow,
+        LEFT_STRAFE = KeyCode.LeftArrow,
+        RIGHT_STRAFE = KeyCode.RightArrow,
+    }
+
+    #region Variables
+
+    public float speed = 3f;
+
+    public bool isDoor = false;
+    private bool isSelected = false;
+
+    public GameObject obstacle;
+
+    public Material[] materials;
+    private Renderer rend;
+
+    #endregion
+
+    #region MovableObstacle Functions
+
+    // Use this for initialization
+    void Start ()
+    {
+        rend = GetComponent<Renderer>();
+        rend.enabled = true;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                if (hit.transform == this.transform)
+                {
+                    if (this.isSelected == false)
+                    {
+                        this.isSelected = true;
+                        rend.sharedMaterial = materials[1];
+                    }
+                    else if (this.isSelected == true)
+                    {
+                        this.isSelected = false;
+                        rend.sharedMaterial = materials[0];
+                    }
+                }
+            }
+        }
+
+        foreach (KEYBOARD_CONTROLS key in Enum.GetValues(typeof(KEYBOARD_CONTROLS)))
+        {
+            if (Input.GetKey((KeyCode)key) && this.isSelected == true)
+            {
+                switch (key)
+                {
+                    // translation movements
+                    case KEYBOARD_CONTROLS.FOWARD:
+                        if (isDoor)
+                        {
+                            obstacle.transform.position += new Vector3(0, Time.deltaTime * speed, 0);
+                            break;
+                        }
+                        obstacle.transform.position -= new Vector3(0, 0, Time.deltaTime * speed);
+                        break;
+                    case KEYBOARD_CONTROLS.BACKWARD:
+                        if (isDoor)
+                        {
+                            obstacle.transform.position -= new Vector3(0, Time.deltaTime * speed, 0);
+                            break;
+                        }
+                        obstacle.transform.position += new Vector3(0, 0, Time.deltaTime * speed);
+                        break;
+                    case KEYBOARD_CONTROLS.LEFT_STRAFE:
+                        if (isDoor)
+                        {
+                            obstacle.transform.position += new Vector3(0, 0, Time.deltaTime * speed);
+                            break;
+                        }
+                        obstacle.transform.position += new Vector3(Time.deltaTime * speed, 0, 0);
+                        break;
+                    case KEYBOARD_CONTROLS.RIGHT_STRAFE:
+                        if (isDoor)
+                        {
+                            obstacle.transform.position -= new Vector3(0, 0, Time.deltaTime * speed);
+                            break;
+                        }
+                        obstacle.transform.position -= new Vector3(Time.deltaTime * speed, 0, 0);
+                        break;
+                }
+            }
+        }
+    }
+
+    #endregion
+}
